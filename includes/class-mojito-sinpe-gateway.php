8eed70a3e9de9f326a1e55b7b310036f8bf5f0ba
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-22 15:25:41 -0600

    fix url

--- includes/class-mojito-sinpe-gateway.php
+++ includes/class-mojito-sinpe-gateway.php
@@ -168,7 +168,8 @@ class Mojito_Sinpe_Gateway extends WC_Payment_Gateway {
 		}
 
 		/*
-		URL: https://www.bccr.fi.cr/sistema-de-pagos/informaci%C3%B3n-general/tarifas-y-comisiones-del-sinpe/comisiones-cobradas-por-las-entidades-financieras/sinpe-m%C3%B3vil
+		Note: Web Archive URL. The real url was removed from Central Bank's SINPE official documentation.
+		URL: https://web.archive.org/web/20210824000600/https://www.bccr.fi.cr/sistema-de-pagos/informaci%C3%B3n-general/tarifas-y-comisiones-del-sinpe/comisiones-cobradas-por-las-entidades-financieras/sinpe-m%C3%B3vil
 		*/
 		$sinpe_banks = array(
 			'none'            => __( 'Select your bank', 'mojito-sinpe' ),
